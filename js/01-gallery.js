1d70d911fa70327d574088dcc440016087e0e9a1
EITalanova <elenabelichenko77@gmail.com>
2023-01-20 17:25:09 -0800

    1 task done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,10 +1,44 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
+const gallery = document.querySelector(".gallery");
 
-// удалить
-console.log(galleryItems);
-// удалить
+const markupGallery = galleryItems
+  .map(
+    (galleryItem) =>
+      `<div class="gallery__item">
+  <a class="gallery__link" href="large-image.jpg">
+    <img
+      class="gallery__image"
+      src='${galleryItem.preview}'
+      data-source='${galleryItem.original}'
+      alt='${galleryItem.description}'
+    />
+  </a>
+</div>`
+  )
+  .join("");
 
+gallery.insertAdjacentHTML("beforeend", markupGallery);
 
-console.log(galleryItems);
+function handlerOpenModal(evt) {
+  evt.preventDefault();
+  window.addEventListener("keydown", handlerCloseModal);
+  if (!evt.target.classList.contains("gallery__image")) {
+    return;
+  }
+  const instance = basicLightbox.create(`
+    <div class="modal">
+        <img src=${evt.target.dataset.source} width="800" height="600">
+    </div>
+`);
+  instance.show();
+
+  function handlerCloseModal(evt) {
+    if (evt.code === "Escape") {
+      instance.close();
+    }
+  }
+}
+
+gallery.addEventListener("click", handlerOpenModal);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,18 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
+const gallery = document.querySelector(".gallery");
+
+const markupGallery = galleryItems
+  .map(
+    (galleryItem) =>
+      `<a class="gallery__item" href="${galleryItem.original}">
+  <img class="gallery__image" src="${galleryItem.preview}" alt="${galleryItem.description}" />
+</a>`
+  )
+    .join("");
+  
+gallery.insertAdjacentHTML('beforeend', markupGallery);
+
+console.log(markupGallery);
 console.log(galleryItems);
